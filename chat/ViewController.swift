b2502dcad436353aff14811df98ecea918d5448c
minutoni <minutoni105@gmail.com>
2019-04-27 12:56:11 +0900

    Initial Commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by 所　紀彦 on 2019/04/27.
+//  Copyright © 2019 所　紀彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
