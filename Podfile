36def6bfdb45c97d5504bc15f6e777e59a603295
minutoni <minutoni105@gmail.com>
2019-05-16 22:39:53 +0900

    まだまだ道は長い。。。

--- Podfile
+++ Podfile
@@ -10,6 +10,7 @@ pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Database'
 pod 'Firebase'
-pod 'JSQMessagesViewController'
 pod 'SVProgressHUD'
+pod 'MessageKit'
+pod 'JSQMessagesViewController'
 end
--- chat/ViewController.swift
+++ chat/ViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 import Firebase
-import JSQMessagesViewController
 import SVProgressHUD
 import Speech
+import FirebaseCore
+import FirebaseDatabase
+//import JSQMessagesViewController
 
-class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
-    var messages: [JSQMessage]?
+
+class ViewController: UIViewController,SFSpeechRecognitionTaskDelegate,UITextFieldDelegate{
+//    var messages: [JSQMessage]?
+//
+//    var incomingBubble: JSQMessagesBubbleImage!
+//    var outgoingBubble: JSQMessagesBubbleImage!
+//    var exitcomingBubble: JSQMessagesBubbleImage!
+//
+//    var incomingAvater: JSQMessagesAvatarImage!
+//    var outgoingAvater: JSQMessagesAvatarImage!
     
-    var incomingBubble: JSQMessagesBubbleImage!
-    var outgoingBubble: JSQMessagesBubbleImage!
-    var exitcomingBubble: JSQMessagesBubbleImage!
+    //========================改変==========
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var messageButton: UIButton!
+    //@IBOutlet var recordButton == messageButton      : UIButton!
+    @IBOutlet weak var nameField: UITextField!
+    var databaseRef:DatabaseReference!
     
-    var incomingAvater: JSQMessagesAvatarImage!
-    var outgoingAvater: JSQMessagesAvatarImage!
+    
     
     var uid:String = UUID().uuidString
     
@@ -34,7 +46,7 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
     let app:AppDelegate = (UIApplication.shared.delegate as! AppDelegate)
     
     //ここから追加＊
-    @IBOutlet var recordButton: UIButton!
+    
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja_JP"))!
     private var recognitionTask: SFSpeechRecognitionTask?
     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
@@ -42,16 +54,27 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //=====================改変
+        //messageField.delegate = self as! UITextFieldDelegate
         
         // Do any additional setup after loading the view.
         
-        //初回時のみ自分のIDを保存しておく、それ以降は使い回し
+        //初回時のみ自分のIDを保存しておく、それ以降は使い回し(uid管理
         if(userDefaults.string(forKey: "uid") != nil){
             uid = userDefaults.string(forKey: "uid")!
+            self.textView.text! = "\(String(describing: self.textView.text))\n\(String(describing: uname)) : \(messageButton)"
         }else{
             userDefaults.set(uid, forKey: "uid")
         }
-        
+//        //セッティングしている。新しいデータが入ってくるのを見張ってる。もしも、新しいデータが入ってきた場合{}内の処理を実行する。
+//        databaseRef.observe(DataEventType.childAdded, with: { snapshot in //セッティングしている。新しいデータが入ってくるのを見張ってる。もしも、新しいデータが入ってきた場合{}内の処理を実行する。
+//            if let name = (snapshot.value! as AnyObject).object(forKey: "name") as? String,
+//                let message = (snapshot.value! as AnyObject).object(forKey: "message") as? String {
+//                self.textView.text! = "\(self.textView.text!)\n\(name) : \(message)"
+//
+//            }
+//        })
+    
         print("自分のuid→\(self.uid)")
         
         //indicatorを回す
@@ -63,15 +86,19 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
         self.app.chatStartFlg = false
         
         //表示される名前(未実装)
-        self.senderDisplayName = "hoge"
+        //self.senderDisplayName = "hoge"
         setupFirebase()
         getRoom()
-        setupChatUi()
+        //setUpChatUi()
         
-        self.messages = []
+        //self.messages = []
         
         //＊
-        recordButton.isEnabled = false
+        messageButton.isEnabled = false
+        
+        //firebaseのfromカラムに入る値
+               //self.senderId = self.uid
+        
         
     }
     
@@ -82,18 +109,18 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
             OperationQueue.main.addOperation {
                 switch status {
                 case .authorized: break   // 許可OK
-                    self.recordButton.isEnabled = true
+                    self.messageButton.isEnabled = true
                     //録音ボタンの設定
-                    self.recordButton.backgroundColor = UIColor.blue
+                    self.messageButton.backgroundColor = UIColor.blue
                 case .denied:       // 拒否
-                    self.recordButton.isEnabled = false
-                    self.recordButton.setTitle("録音許可なし", for: .disabled)
+                    self.messageButton.isEnabled = false
+                    self.messageButton.setTitle("録音許可なし", for: .disabled)
                 case .restricted:   // 限定
-                    self.recordButton.isEnabled = false
-                    self.recordButton.setTitle("このデバイスでは無効", for: .disabled)
+                    self.messageButton.isEnabled = false
+                    self.messageButton.setTitle("このデバイスでは無効", for: .disabled)
                 case .notDetermined:// 不明
-                    self.recordButton.isEnabled = false
-                    self.recordButton.setTitle("録音機能が無効", for: .disabled)
+                    self.messageButton.isEnabled = false
+                    self.messageButton.setTitle("録音機能が無効", for: .disabled)
                 }
             }
         }
@@ -116,11 +143,12 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
     }
     
     func getRoom(){
-        let user = ["name": senderDisplayName,
-                    "inRoom": "0",
-                    "waitingFlg": "0"] as [String : Any]
+//        let user = ["name":hoge,
+//            //senderDisplayName,
+//                    "inRoom": "0",
+//                    "waitingFlg": "0"] as [String : Any]
         
-        userRef.child(self.uid).setValue(user)
+        userRef.child(self.uid).setValue(databaseRef)
         
         //一回だけwaitingFlgが1のユーザーを取得
         userRef.queryOrdered(byChild: "waitingFlg").queryEqual(toValue: "1").observeSingleEvent(of: .value,with: { (snapshot) in
@@ -191,9 +219,11 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
             let name          = snapshotValue["name"] as! String
             
             print("display名前→\(name)")
-            let message       = JSQMessage(senderId: sender, displayName: name, text: text)
-            self.messages?.append(message!)
-            self.finishReceivingMessage()
+//            let message       = getMessages(senderId: sender,
+//                                            displayName: name,
+//                                            text: text)
+//            self.messages?.append(message!)
+//            self.finishReceivingMessage()
         })
     }
     
@@ -251,22 +281,7 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
         getMessages()
         
     }
-    
-    func setupChatUi() {
-        inputToolbar!.contentView!.leftBarButtonItem = nil
-        automaticallyScrollsToMostRecentMessage = true
-        
-        //firebaseのfromカラムに入る値
-        self.senderId = self.uid
-        
-        self.collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
-        self.collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
-        
-        let bubbleFactory = JSQMessagesBubbleImageFactory()
-        self.incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(with: UIColor.gray)
-        self.outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleGreen())
-        self.exitcomingBubble = bubbleFactory?.incomingMessagesBubbleImage(with:  UIColor.jsq_messageBubbleRed())
-    }
+
     
     
     //別の画面に遷移する直前に実行される処理
@@ -287,58 +302,37 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
         if(self.app.roomId != "0"){
             //相手に退出のメッセージを送るようにする
             let endMsg = "~相手が退出したよ！~"
-            sendTextToDb(text: endMsg,exitFlg: true)
+            //sendTextToDb(text: endMsg,exitFlg: true)
             self.app.roomId = "0"
             
         }
     }
     
-    //=================================↓JSQMessageの色々==========================================//
+    //メッセージの送信
+//    override func didPressSend(_ button: UIButton!, withMessageText text: String!, //senderId: String!, senderDisplayName: String!,
+//        date: Date!) {
+//        self.finishSendingMessage(animated: true)
+//
+//        if(self.app.chatStartFlg! == true){
+//
+//            startRecording(text:text)
+//            //*ここからsendTextToDbへデータを送る
+//            sendTextToDb(text: text)
+//            self.textView.text = Result.bestTranscription.formattedString
+//
+//            let messageData = ["name": nameField.text!, "message": ] //messageField.text!
+//            // firebaseにdictionary型を保存するためデータを作成！
+//            databaseRef.childByAutoId().setValue(messageData) // データを保存！
+//            textView.resignFirstResponder() //キーボードを閉じる
+//            //messageField.text = "" //最終的にはtextFieldを空にする。
+//            return true
+//
+//        }else{
+//            print("チャット相手を検索中です,,,")
+//        }
+//    }
     
-    //メッセージの送信  
-    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
-        self.finishSendingMessage(animated: true)
-        
-        if(self.app.chatStartFlg! == true){
-            //*ここからsendTextToDbへデータを送る
-            sendTextToDb(text: text)
-            //self.textView.text = Result.bestTranscription.formattedString
-        }else{
-            print("チャット相手を検索中です,,,")
-        }
-    }
-    
-    
-    
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
-        return self.messages?[indexPath.item]
-    }
-    
-    //バブルの色を返す。
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
-        let message = self.messages?[indexPath.item]
-        
-        if message?.senderId == self.senderId {
-            return self.outgoingBubble
-        } else if message?.senderId == "exit"{
-            return self.exitcomingBubble
-        }
-        return self.incomingBubble
-    }
-    
-    //アバター（サムネを返す）
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!,
-                                 avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
-        let message = self.messages?[indexPath.item]
-        if message?.senderId == self.senderId {
-            return self.outgoingAvater
-        }
-        return self.incomingAvater
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return (self.messages?.count)!
-    }
+
     
     
 //====================================================＊===============================================
@@ -349,13 +343,13 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
     public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
         if available {
             // 利用可能になったら、録音ボタンを有効にする
-            recordButton.isEnabled = true
-            recordButton.setTitle("Start Recording", for: [])
-            recordButton.backgroundColor = UIColor.blue
+            messageButton.isEnabled = true
+            messageButton.setTitle("Start Recording", for: [])
+            messageButton.backgroundColor = UIColor.blue
         } else {
             // 利用できないなら、録音ボタンは無効にする
-            recordButton.isEnabled = false
-            recordButton.setTitle("現在、使用不可", for: .disabled)
+            messageButton.isEnabled = false
+            messageButton.setTitle("現在、使用不可", for: .disabled)
         }
     }
     
@@ -385,12 +379,15 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
             
             if let result = result{
                 //ここ！音声データが送られてくる場所？
-                //self.textView.text = result.bestTranscription.formattedString
+                self.textView.text = result.bestTranscription.formattedString
                 
 //                let post:Dictionary<String, Any>? = ["from":tmpSenderId,
 //                                                     "name":senderDisplayName(),
 //                                                     "text":text]
                 isFinal = result.isFinal
+                //self.sendTextToDb(text: result.bestTranscription.formattedString)
+                
+                
             }
             
             if Error.self != nil || isFinal{
@@ -400,9 +397,9 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
                 self.recognitionRequest = nil
                 self.recognitionTask = nil
                 
-                self.recordButton.isEnabled = true
-                self.recordButton.setTitle("start recording", for: [])
-                self.recordButton.backgroundColor = UIColor.blue
+                self.messageButton.isEnabled = true
+                self.messageButton.setTitle("start recording", for: [])
+                self.messageButton.backgroundColor = UIColor.white
                 
             }
         }
@@ -421,40 +418,53 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
     }
     
     
-    func sendTextToDb(text: String,exitFlg:Bool = false){
+   func sendTextToDb(text: String,exitFlg:Bool = false){
         //データベースへの送信（後述）
         let rootRef = Database.database().reference()
-        
-        var tmpSenderId = senderId as String
+    
+        //var tmpSenderId = senderId as String
         if(exitFlg){
-            tmpSenderId = "exit"
+            //tmpSenderId = "exit"
         }
         
-        let post:Dictionary<String, Any>? = ["from":tmpSenderId,
-                                             "name":senderDisplayName(),
-                                             "text":text]
-        
+//        let post:Dictionary<String, Any>? = ["from":tmpSenderId,
+//                                             "name"://senderDisplayName(),
+//                                             "text":text]
+    
+//    let messageData = ["name": UITextField.text,
+//                       "message": //self.messageField.text!
+//        ] as [String : Any]
+    self.textView.resignFirstResponder()
+    //self.messa.text = ""
+    //return true
         
         let postRef = rootRef.child("rooms").child(self.app.roomId!).childByAutoId()
-        postRef.setValue(post)
-        
+        //postRef.setValue(post)
+    
+    
+    
     }
     
-      func recordButtonTapped() {
+      @IBAction func recordButtonTapped() {
         if audioEngine.isRunning {
             // 音声エンジン動作中なら停止
             audioEngine.stop()
             recognitionRequest?.endAudio()
-            recordButton.isEnabled = false
-            recordButton.setTitle("Stopping", for: .disabled)
-            recordButton.backgroundColor = UIColor.lightGray
+            messageButton.isEnabled = false
+            messageButton.setTitle("Stopping", for: .disabled)
+           messageButton.backgroundColor = UIColor.lightGray
             return
         }
         // 録音を開始する
         try! startRecording()
-        recordButton.setTitle("認識を完了する", for: [])
-        recordButton.backgroundColor = UIColor.red
+        messageButton.setTitle("認識を完了する", for: [])
+        messageButton.backgroundColor = UIColor.red
     }
+    
+    //=================================================代用案(カメラボタンを変えてみる)==================
+//    override func didPressAccessoryButton(_ sender: UIButton!) {
+//        recordButtonTapped()
+//    }
     
     
     /*
@@ -466,6 +476,66 @@ class ViewController: JSQMessagesViewController,SFSpeechRecognitionTaskDelegate{
      // Pass the selected object to the new view controller.
      }
      */
+    
+    //=================================↓JSQMessageの色々==========================================//
+    
+    
+    
+    
+    
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+//        return self.messages?[indexPath.item]
+//    }
+//
+//    //バブルの色を返す。
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+//        let message = self.messages?[indexPath.item]
+//
+//        if message?.senderId == self.senderId {
+//            return self.outgoingBubble
+//        } else if message?.senderId == "exit"{
+//            return self.exitcomingBubble
+//        }
+//        return self.incomingBubble
+//    }
+//
+//    //アバター（サムネを返す）
+//    override func collectionView(_ collectionView: JSQMessagesCollectionView!,
+//                                 avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+//        let message = self.messages?[indexPath.item]
+//        if message?.senderId == self.senderId {
+//            return self.outgoingAvater
+//        }
+//        return self.incomingAvater
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return (self.messages?.count)!
+//    }
+//
+//    func setUpChatUi() {
+//        //ツールバーの設定
+//        //inputToolbar!.contentView!.leftBarButtonItem = nil
+//        inputToolbar!.contentView!.rightBarButtonItem = nil
+//        automaticallyScrollsToMostRecentMessage = true
+//
+//        //firebaseのfromカラムに入る値
+//        self.senderId = self.uid
+//
+//        //jself.collectionView.backgroundView = JSQPhotoMediaItem(image: UIImage(named: "p0121_l.png"))
+//
+//        self.collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+//        self.collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+//
+//        let bubbleFactory = JSQMessagesBubbleImageFactory()
+//        self.incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(
+//            with: UIColor.gray)
+//        self.outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(
+//            with: UIColor.jsq_messageBubbleGreen())
+//        self.exitcomingBubble = bubbleFactory?.incomingMessagesBubbleImage(
+//            with: UIColor.jsq_messageBubbleRed())
+//
+//    }
 }
 
 
